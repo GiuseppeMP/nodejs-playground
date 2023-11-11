2ab9fca2d48b58c7e912efe10c8fd4a7b30be866
Peppe <giuseppemath@gmail.com>
2023-11-11 18:17:23 -0300

    (chore): project setup

---
+++ src/Exemplo.test.ts
@@ -0,0 +1,8 @@
+import Exemplo from "./Exemplo";
+
+describe('Descrição da suite de testes...', () => {
+    it('teste ....', () => {
+        const exemplo = new Exemplo()
+        expect(exemplo).toBeDefined()
+    })
+})
